674e4ca0827aa10c928656416bb884119a58a638
phuc.nguyen <phuc.nguyen@nexpando.com>
2024-04-08 15:15:19 +0700

    edit url when login success and animation of signin signup

--- Chat-App-FE/src/App.js
+++ Chat-App-FE/src/App.js
@@ -1,15 +1,15 @@
-import { Routes, Route } from 'react-router-dom';
-import TableUsers from './components/TableUsers';
-import Login from './pages/Login';
-import Chat from './pages/Chat';
-import Register from './pages/Register';
-import ResetPassword from './pages/ResetPassword';
+import { Routes, Route } from "react-router-dom";
+import TableUsers from "./components/TableUsers";
+import Login from "./pages/Login";
+import Chat from "./pages/Chat";
+import Register from "./pages/Register";
+import ResetPassword from "./pages/ResetPassword";
 function App() {
   return (
     <div className="app-container">
       <TableUsers />
       <Routes>
-        <Route path="/messages" element={<Chat />} />
+        <Route path="/messages/:id" element={<Chat />} />
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/resetpassword/:id" element={<ResetPassword />} />
