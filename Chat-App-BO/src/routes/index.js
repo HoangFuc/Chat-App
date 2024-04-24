e21beaba1ff19680679a1044d2e6863f464550a7
hiimvanty <hiimvanty@gmail.com>
2024-04-24 13:58:50 +0700

    chatRoom UI and Delete chatRoom in Admin

--- Chat-App-BO/src/routes/index.js
+++ Chat-App-BO/src/routes/index.js
@@ -1,10 +1,16 @@
 
 const userRouter = require('./user.route');
 const detailRouter = require('./detail.route');
+const chatRoomRouter = require('./chatRoom.route');
+
 function route(app) {
     //Dinh tuyen trang list User la /api/userList
     app.use('/api/userList/detail', detailRouter);
     app.use('/api/userList', userRouter);
+
+    //DInh tuyen trang list ChatRoom la /api/chatRoomList
+    app.use('/api/chatRoomList', chatRoomRouter);
+
 }
 
 module.exports = route;
--- Chat-App-BO/src/routes/user.route.js
+++ Chat-App-BO/src/routes/user.route.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const userController = require("../app/controllers/userController");
+const authorController = require("../../../Chat-App-BE/controllers/authController");
 
 //Create
 router.get('/create', userController.create);
