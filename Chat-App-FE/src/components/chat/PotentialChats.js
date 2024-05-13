7037542a2766aac60147b0b40688b7eab5c1e1e6
Trà Thanh Tú <tttrathanhtu2002@gmail.com>
2024-05-13 18:44:03 +0700

    fix loop createChat

--- Chat-App-FE/src/components/chat/PotentialChats.js
+++ Chat-App-FE/src/components/chat/PotentialChats.js
@@ -1,21 +1,33 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { ChatContext } from '../../context/ChatContext';
 import { AuthContext } from '../../context/AuthContext';
 
 const PotentialChats = () => {
   const { user } = useContext(AuthContext);
   const { potentialChats, createChat, onlineUser } = useContext(ChatContext);
-
+  const [clickedIds, setClickedIds] = useState([]);
+  console.log('click', clickedIds);
   return (
     <>
       <div className="all-users">
         {potentialChats &&
           potentialChats.map((u, index) => {
+            console.log('uid', u._id);
+            const handeleCreateChat = () => {
+              if (clickedIds.includes(u._id)) {
+                return false;
+              }
+
+              setClickedIds([...clickedIds, u._id]);
+
+              createChat(user._id, u._id);
+            };
+
             return (
               <div
                 className="single-user"
                 key={index}
-                onClick={() => createChat(user._id, u._id)}
+                onClick={handeleCreateChat}
               >
                 {u.username}
                 <span
